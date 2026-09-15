14d9390d8602c54c72c2a70251cc41095e31e76a
Indumathi-V <54302074+Indumathi-V@users.noreply.github.com>
2026-09-15 12:21:18 +0530

    Create solution.sql

---
+++ Program14_Sum_Two_Numbers/solution.sql
@@ -0,0 +1,18 @@
+USE CollegeDB;
+
+DROP PROCEDURE IF EXISTS CalculateSum;
+
+DELIMITER $$
+
+CREATE PROCEDURE CalculateSum()
+BEGIN
+    -- Declare two variables
+    -- Assign values
+    -- Calculate and display the sum
+
+END $$
+
+DELIMITER ;
+
+-- Execute the procedure
+CALL CalculateSum();
