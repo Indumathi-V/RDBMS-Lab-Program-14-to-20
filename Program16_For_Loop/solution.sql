da1c24d273d3987a1896812528967973a2d07aaf
Indumathi-V <54302074+Indumathi-V@users.noreply.github.com>
2026-09-15 12:24:24 +0530

    Create solution.sql

---
+++ Program16_For_Loop/solution.sql
@@ -0,0 +1,18 @@
+USE CollegeDB;
+
+DROP PROCEDURE IF EXISTS DisplayNumbers;
+
+DELIMITER $$
+
+CREATE PROCEDURE DisplayNumbers()
+BEGIN
+
+    -- Declare counter variable
+
+    -- Write a loop to display numbers from 1 to 10
+
+END $$
+
+DELIMITER ;
+
+CALL DisplayNumbers();
