d2a187317501c2114fe0ecbee4d3693fbe5ea663
Indumathi-V <54302074+Indumathi-V@users.noreply.github.com>
2026-09-15 12:28:48 +0530

    Create solution.sql

---
+++ Program19_Student_Cursor/solution.sql
@@ -0,0 +1,26 @@
+USE CollegeDB;
+
+DROP PROCEDURE IF EXISTS DisplayStudents;
+
+DELIMITER $$
+
+CREATE PROCEDURE DisplayStudents()
+BEGIN
+
+    -- Declare variables
+
+    -- Declare cursor
+
+    -- Declare NOT FOUND handler
+
+    -- Open cursor
+
+    -- Fetch records using a loop
+
+    -- Close cursor
+
+END $$
+
+DELIMITER ;
+
+CALL DisplayStudents();
